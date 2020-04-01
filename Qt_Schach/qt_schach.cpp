0472c8f50b44f37a341482e1ca60f9e6a56f96ce
Otto Plitz <otto.plitz@hotmail.de>
2020-04-01 14:05:09 +0200

    Start

--- Qt_Schach/qt_schach.cpp
+++ Qt_Schach/qt_schach.cpp
@@ -4,4 +4,7 @@ Qt_Schach::Qt_Schach(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	actions();
+	this->statusBar()->setSizeGripEnabled(false);
+	setWindowFlags(Qt::Dialog | Qt::MSWindowsFixedSizeDialogHint);
 }
